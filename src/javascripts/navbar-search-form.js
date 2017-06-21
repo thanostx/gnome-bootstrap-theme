1cfbe5f8d181bc3cef576d1424cb4c9e4e8fe57f
Tom Tryfonidis <tomtryf@gnome.org>
2017-06-21 15:11:08 +0300

    Rework searchbar markup and functionality
    Trigger the parent div of the searchbar, allowing to open the actual input.

--- src/javascripts/navbar-search-form.js
+++ src/javascripts/navbar-search-form.js
@@ -1,6 +1,6 @@
 var searchbar = $('.js-search');
 $('.js-search-toggle').on('click', function () {
-    searchbar.parent().toggleClass('is-visible'),
+    searchbar.parent().closest('div').toggleClass('is-visible'),
     setTimeout(function () {
       searchbar.focus()
     }, 350)
